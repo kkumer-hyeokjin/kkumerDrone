2b560e928c4fdc68b5915f65ed153174af4ee7a2
kkumer-0418 <qkrgurwls98@naver.com>
2020-03-23 20:18:52 +0900

    Initial Commit

---
+++ kkumerDrone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  kkumerDrone
+//
+//  Created by 박혁진 on 2020/03/23.
+//  Copyright © 2020 kkumercompany. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
